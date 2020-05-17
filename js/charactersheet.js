06cf45d4325df16371a5889d6ce86986f7d168db
noah.sennett <noah.sennett@gmail.com>
2020-05-17 14:07:55 -0400

    Finished background json. Started functionality for picking skills from background.

--- js/charactersheet.js
+++ js/charactersheet.js
@@ -9,6 +9,7 @@ var tempAttrScore="";
 
 var tempSelections=["", "", "", "", "", ""];
 
+var remainingRolls=3;
 
 function rollDie(sides=6){
     return 1+Math.floor(sides*Math.random());
@@ -187,8 +188,10 @@ function resetTemps(){
 }
 
 let backgroundURL = 'https://noah-sennett.github.io/swn-character-creator/js/backgrounds.json';
-
 var backgrounds;
+
+let skillURL = 'https://noah-sennett.github.io/swn-character-creator/js/skill.json';
+var skills;
 
 function loadBackgrounds(){
     let request = new XMLHttpRequest();
@@ -233,4 +236,286 @@ function displayBackground(background) {
     }
 }
 
+function populateLearning(background){
+    let elemLearning = document.getElementById("learning");
+    while(elemLearning.options.length>0){
+	elemLearning.remove(0);
+    }
+    
+    if (background!=""){
+	var learningOptions = backgrounds[background]["learning"];
+	for (var opt of learningOptions){
+	    var option = document.createElement("option");
+	    option.text = opt;
+	    option.value = opt;
+	    elemLearning.add(option);
+	}
+    }
+}
 
+function populateGrowth(background){
+    let elemGrowth = document.getElementById("growth");
+    while(elemGrowth.options.length>0){
+	elemGrowth.remove(0);
+    }
+    
+    if (background!=""){
+	var growthOptions = backgrounds[background]["growth"];
+	for (var opt of growthOptions){
+	    var option = document.createElement("option");
+	    option.text = opt;
+	    option.value = opt;
+	    elemGrowth.add(option);
+	}
+    }
+}
+
+function loadSkills(){
+    let request = new XMLHttpRequest();
+    request.open('GET', skillURL);
+
+    request.responseType = 'json';
+
+
+    request.onload = function() {
+	skills = request.response;
+	generateSkillTable(skills);
+    }
+
+    request.send();
+}
+
+
+function generateSkillTable(skills){
+    var body = document.body; 
+    var tbl  = document.createElement('table');
+    tbl.style.width  = '600px';
+    tbl.style.border = '1px solid black';
+
+    var skillKeys=Object.keys(skills);
+
+    for(var i = 0; i < 9; i++){
+        var tr = tbl.insertRow();
+        for(var j = 0; j < 3; j++){
+            var td = tr.insertCell();
+	    var skillBlock = document.createElement('div');
+	    skillBlock.setAttribute("class","skillBlock");
+	    
+	    var skillName = document.createElement('div');
+	    var skillRank = document.createElement('div');
+	    var skillTotal = document.createElement('div');
+
+	    var skillKey;
+	    var node;
+	    var tooltipNode;
+	    
+	    if (j < 2 || i==0){
+		skillKey = skillKeys[i+j*9];
+	    }
+	    else{
+		skillKey = skillKeys[i-2+j*9];
+	    }
+	    
+	    if ((j<2||i==0)|| i>2){
+			    
+		skillName.setAttribute("id",skillKey+'_label');
+		skillName.setAttribute("class","tooltip");
+		skillTotal.setAttribute("id",skillKey+'_total');
+
+		node = document.createTextNode(skills[skillKey]["name"]);
+
+		tooltipNode = document.createElement('span');
+		tooltipNode.setAttribute("class","tooltiptext");
+		tooltipNode.innerHTML = skills[skillKey]["description"];
+
+		skillName.appendChild(node);
+		skillName.appendChild(tooltipNode);
+	    }
+	    else{
+		node = document.createTextNode("");
+		skillName.appendChild(node);
+	    }
+	    
+  
+	    if ((j<2||i==0)|| i>2){
+		
+		var box0=document.createElement('input');
+		var box1=document.createElement('input');
+		var box2=document.createElement('input');
+		var box3=document.createElement('input');
+		var box4=document.createElement('input');
+
+		var boxes = [box0, box1, box2, box3, box4];
+
+		for (var box of boxes){
+		    box.setAttribute("type","checkbox");
+		    box.setAttribute("id",skillKey+'_rank_box_'+parseInt(boxes.indexOf(box)));
+		    box.setAttribute("onchange","updateSkillBoxes(this.id);");
+		    skillRank.append(box);
+		}
+		
+		var tot=document.createTextNode("");
+		
+		skillTotal.append(tot);
+	    }
+
+	    
+	    skillBlock.appendChild(skillName);
+	    skillBlock.appendChild(skillRank);
+	    skillBlock.appendChild(skillTotal);
+
+	    skillBlock.setAttribute("id",skillKey+'_box');
+	    
+            td.appendChild(skillBlock);
+	    //                td.style.border = '1px solid black';
+        
+        }
+    }
+    body.appendChild(tbl);
+}
+
+function updateSkillBoxes(boxID){
+    var idPrefix = boxID.slice(0,-1);
+    var idSuffix = boxID.charAt(boxID.length-1);
+
+    var elemBox0 = document.getElementById(idPrefix+'0');
+    var elemBox1 = document.getElementById(idPrefix+'1');
+    var elemBox2 = document.getElementById(idPrefix+'2');
+    var elemBox3 = document.getElementById(idPrefix+'3');
+    var elemBox4 = document.getElementById(idPrefix+'4');
+
+    var elemTotal = document.getElementById(boxID.slice(0,-10)+'total');
+    
+    var elemBoxes = [elemBox0, elemBox1, elemBox2, elemBox3, elemBox4];
+    
+    if (elemBoxes[parseInt(idSuffix)].checked){
+	if (elemBox4.checked) elemBox3.checked=true;
+	if (elemBox3.checked) elemBox2.checked=true;
+	if (elemBox2.checked) elemBox1.checked=true;
+	if (elemBox1.checked) elemBox0.checked=true;
+	
+	elemTotal.innerHTML = idSuffix;
+    }
+    else{
+	if (!(elemBox0.checked)) elemBox1.checked=false;
+	if (!(elemBox1.checked)) elemBox2.checked=false;
+	if (!(elemBox2.checked)) elemBox3.checked=false;
+	if (!(elemBox3.checked)) elemBox4.checked=false;
+
+	if (idSuffix != '0'){
+	    elemTotal.innerHTML = parseInt(idSuffix)-1;
+	}
+	else{
+	    elemTotal.innerHTML = "";
+	}
+	
+    }
+    
+}
+
+function fixFreeSkill(background){
+    var allInputs = document.getElementsByTagName("input");
+    for (var input of allInputs){
+	input.disabled=false;
+	input.checked=false;
+    }
+
+    var skillKeys = Object.keys(skills);
+    for (var skillKey of skillKeys){
+	var elem = document.getElementById(skillKey+'_total');
+	elem.innerHTML="";
+    }
+    
+    
+    if (background != ""){
+	var freeSkill = backgrounds[background]["free_skill"].slice(0,-2).toLowerCase();
+	var elemBox0 = document.getElementById(freeSkill+'_rank_box_0');
+	var elemTotal = document.getElementById(freeSkill+'_total');
+	elemBox0.checked=true;
+	elemBox0.disabled=true;
+	elemTotal.innerHTML = "0";
+    }
+}
+
+function resetSelect(elem){
+    for (var option of elem.options){
+	option.selected=false;
+    }
+}
+
+function showGrowthButtons(){
+    let elem1=document.getElementById("growth_button");
+    let elem2=document.getElementById("learning_button");
+    elem1.style.display= 'inline';
+    elem2.style.display= 'inline';
+
+    resetSelect(document.getElementById("growth"));
+    resetSelect(document.getElementById("learning"));
+
+    let elemLearning=document.getElementById("learning");
+    elemLearning.setAttribute("disabled","true");
+
+    
+    remainingRolls = 3;
+    elem1.removeAttribute("disabled")
+    elem2.removeAttribute("disabled")
+}
+
+function hideGrowthButtons(){
+    let elem1=document.getElementById("growth_button");
+    let elem2=document.getElementById("learning_button");
+    elem1.style.display= 'none';
+    elem2.style.display= 'none';
+
+    resetSelect(document.getElementById("growth"));
+    resetSelect(document.getElementById("learning"));
+}
+
+function enableLearningChoices(){
+    let elemLearning=document.getElementById("learning");
+    elemLearning.removeAttribute("disabled");
+}
+
+function rollGrowth(){
+    let roll = rollDie(6);
+    let elem = document.getElementById("growth");
+    let elem2 = document.getElementById("learning");
+    elem.options[roll-1].selected = "true";
+
+    remainingRolls--;
+    if (remainingRolls==0){
+	let elemGrowthButton = document.getElementById("growth_button");
+	let elemLearningButton = document.getElementById("learning_button");
+	elemGrowthButton.setAttribute("disabled","true");
+	elemLearningButton.setAttribute("disabled","true");
+    }
+}
+
+function rollLearning(){
+    let roll = rollDie(8);
+    let elem = document.getElementById("learning");
+    elem.options[roll-1].selected = "true";
+
+    remainingRolls--;
+    if (remainingRolls==0){
+	let elemGrowthButton = document.getElementById("growth_button");
+	let elemLearningButton = document.getElementById("learning_button");
+	elemGrowthButton.setAttribute("disabled","true");
+	elemLearningButton.setAttribute("disabled","true");
+    }   
+}
+
+
+function enableSkillChoiceButtons(background){
+    let elemPick=document.getElementById("pick_skill");
+    let elemRoll=document.getElementById("roll_skill");
+
+    if (background==""){
+	elemPick.setAttribute("disabled","true");
+	elemRoll.setAttribute("disabled","true");
+    }
+    else{
+	elemPick.removeAttribute("disabled");
+	elemRoll.removeAttribute("disabled");
+    }
+}
