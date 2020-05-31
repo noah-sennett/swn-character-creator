7c71c4d17b6e79fccd85c79093adfc402d3118c6
noah.sennett <noah.sennett@gmail.com>
2020-05-31 08:23:47 -0400

    Edits

--- js/charactersheet.js
+++ js/charactersheet.js
@@ -3401,7 +3401,7 @@ function fillOutSheetPage1_jspdf(){
 	      
 
 	    
-            var doc = new jsPDF("landscape", "letter"); // using defaults: orientation=portrait, unit=mm, size=A4
+            var doc = new jsPDF("landscape", "cm", "letter"); // using defaults: orientation=portrait, unit=mm, size=A4
             var width = doc.internal.pageSize.width;    
             var height = doc.internal.pageSize.height;
 	    console.log(width);
@@ -3413,13 +3413,5 @@ function fillOutSheetPage1_jspdf(){
     });
     
     
-}
-
-/**
- * Display a base64 URL inside an iframe in another window.
- */
-function debugBase64(base64URL){
-    var win = window.open();
-    win.document.write('<iframe src="' + base64URL  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
 }
 
